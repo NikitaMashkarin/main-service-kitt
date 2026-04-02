268c56f9041054d8216672fdcf9c68da61f45722
NikitaMashkarin <mashkarinnikita@yandex.ru>
2026-04-02 14:27:01 +0300

    Initial commit

---
+++ src/main/java/ru/practicum/mainservicekitt/MainServiceKittApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.mainservicekitt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServiceKittApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainServiceKittApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/practicum/mainservicekitt/MainServiceKittApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.mainservicekitt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServiceKittApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
