c7cb9b633aece362532a6b546c76313063745f3a
NikitaMashkarin <mashkarinnikita@yandex.ru>
2026-04-02 16:53:54 +0300

    Сделать локации

---
+++ src/main/java/ru/practicum/mainservicekitt/location/LocationMapper.java
@@ -0,0 +1,8 @@
+package ru.practicum.mainservicekitt.location;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface LocationMapper {
+}
